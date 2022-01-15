59217088629e04b7dfa19a5512809c2b95b2fad9
Federico Vergara <hu.ce.ro@gmail.com>
2022-01-14 23:49:42 -0300

    Probando proceso por lote

---
+++ scripting/FacturacionLote.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'modules/pedidovendedor/model.php';
+
+
+class FacturacionLote() {
+	function proceso_lote($arg) {
+		$pedidovendedor_id = $arg;
+		$pvm = new PedidoVendedor();
+		$pvm->pedidovendedor_id = $pedidovendedor_id;
+		$pvm->get();
+		$pvm->estadopedido = 2;
+		$pvm->save();
+	}	
+}
+
+$temp_id = filter_input(INPUT_GET, 'pedidovendedor_id');
+$flc = new FacturacionLote();
+$flc->proceso_lote($temp_id);
+?>
