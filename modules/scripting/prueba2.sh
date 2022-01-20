4fae58d10fe8c8512851820bbb386d9a3ba63a8b
Federico Vergara <hu.ce.ro@gmail.com>
2022-01-20 19:11:23 -0300

    Arreglando filtro de cuentas corrientes

--- modules/scripting/prueba2.sh
+++ modules/scripting/prueba2.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 ##EJECUTO CONSULTA SQL Y LA GUARDO EN VARIABLE
-echo 'Hola'
+pedidos_ids=$(mysql  -u Takodana -pn0rt3d15tr1buc10n35 dh.tordo.prod -h localhost -N < getPedidoVendedor.sql &)
+for id in "$pedidos_ids"
+do
+	echo "$id"
+done
+
 exit
