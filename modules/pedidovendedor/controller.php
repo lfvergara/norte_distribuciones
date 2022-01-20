a63ccb0f1c406aebd85881e6d22b8dc0d877b3ad
Federico Vergara <hu.ce.ro@gmail.com>
2022-01-20 18:37:38 -0300

    Arreglando filtro de cuentas corrientes

--- modules/pedidovendedor/controller.php
+++ modules/pedidovendedor/controller.php
@@ -1061,7 +1061,7 @@ class PedidoVendedorController {
 	}
 
 	function ejecuta_proceso_lote() {
-		shell_exec("cd /srv/websites/norte_distribuciones/modules/scripting/prueba.sh");
+		shell_exec("cd /srv/websites/norte_distribuciones/modules/scripting/");
 		shell_exec("sh ./prueba.sh");
 		header("Location: " . URL_APP . "/pedidovendedor/prepara_lote_vendedor/2");
 	}
