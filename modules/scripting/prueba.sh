038b0ecd4a0c07f2fee039586e789d65b5e1735c
Federico Vergara <hu.ce.ro@gmail.com>
2022-01-20 19:42:38 -0300

    Arreglando filtro de cuentas corrientes

--- modules/scripting/prueba.sh
+++ modules/scripting/prueba.sh
@@ -2,12 +2,13 @@
 
 ##EJECUTO CONSULTA SQL Y LA GUARDO EN VARIABLE
 cd /srv/websites/norte_distribuciones/modules/scripting
+
 OIFS="$IFS" ; IFS=$'\n' ; oset="$-" ; set -f
 while IFS=$OIFS read -a line 
 do
 	pedido_id=${line[0]}
-	wget -q -O - "https://www.distribucionesnorte.com.ar/norte_distribuciones/pedidovendedor/proceso_lote/$pedido_id"
-	#echo "$pedido_id"
+	#wget -q -O - "https://www.distribucionesnorte.com.ar/norte_distribuciones/pedidovendedor/proceso_lote/$pedido_id"
+	echo "$pedido_id"
 done < <(mysql -u Takodana -pn0rt3d15tr1buc10n35 dh.tordo.prod -h localhost -N < getPedidoVendedor.sql &)
 
 exit
