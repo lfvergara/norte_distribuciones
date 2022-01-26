8d8fdfa228daf9ea3b81d18fb4868be1e3d3851c
Federico Vergara <hu.ce.ro@gmail.com>
2022-01-25 23:29:51 -0300

    Proceso por lote

--- modules/pedidovendedor/controller.php
+++ modules/pedidovendedor/controller.php
@@ -1257,7 +1257,7 @@ class PedidoVendedorController {
 		if ($tipofactura == 1 OR $tipofactura == 3) {
 			try {
 			    //$this->facturar_afip_argumento($egreso_id);			    
-				$egreso_id = $arg;
+				$egreso_id = $egreso_id;
 				$em = new Egreso();
 				$em->egreso_id = $egreso_id;
 				$em->get();
@@ -1271,7 +1271,7 @@ class PedidoVendedorController {
 				$from = "egresodetalle ed INNER JOIN producto p ON ed.producto_id = p.producto_id INNER JOIN productounidad pu ON p.productounidad = pu.productounidad_id";
 				$where = "ed.egreso_id = {$egreso_id}";
 				$egresodetalle_collection = CollectorCondition()->get('EgresoDetalle', $where, 4, $from, $select);
-				print_r($tfm);exit;
+
 				require_once "tools/facturaAFIPProcesoLoteTool.php";
 				$afip_tool = new FacturaAFIPProcesoLoteTool();
 				$resultadoAFIP = $afip_tool->facturarProcesoLoteAFIP($com, $tfm, $em, $egresodetalle_collection);
@@ -1393,9 +1393,9 @@ class PedidoVendedorController {
 	}
 
 	function ejecuta_proceso_lote() {
-		$out = shell_exec("modules/scripting/prueba.sh");
-		print_r($out);exit;
-		//shell_exec("modules/scripting/prueba.sh");
+		//$out = shell_exec("modules/scripting/prueba.sh");
+		//print_r($out);exit;
+		shell_exec("modules/scripting/prueba.sh");
 		header("Location: " . URL_APP . "/pedidovendedor/prepara_lote_vendedor/2");
 	}
 }
