715a4b5d2afc42952c9c740d1520db815f79c560
Federico Vergara <hu.ce.ro@gmail.com>
2022-02-10 21:52:34 -0300

    Ordenando por ubicación

--- tools/excelreport.php
+++ tools/excelreport.php
@@ -154,7 +154,7 @@ class ExcelReport extends View {
         //$color_temp = 'second_info_style';
         foreach ($array_exportacion as $registro) {
           foreach ($registro as $clave=>$valor) {
-            if ($registo[3] == 'CONTADO') {
+            if ($registo[$clave][3] == 'CONTADO') {
                 //$color = $registro[1];
                 //$breack_row_temp = ($registro[0] != '') ? $registro[0] : $breack_row_temp;
                 $posicion = $this->abecedario[$clave].$l;
