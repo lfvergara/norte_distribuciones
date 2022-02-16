b1f3f825f37744ec67211d358d75238dd251917a
Federico Vergara <hu.ce.ro@gmail.com>
2022-02-16 18:04:04 -0300

    Modificando notación de los números

--- modules/reporte/controller.php
+++ modules/reporte/controller.php
@@ -290,9 +290,9 @@ class ReporteController {
 		}
 
 		$suma_total_compras = $suma_importe_compras_factura + $suma_importe_compras_remito;
-		
+		$estado_actual = ($total_facturado_int + $stock_valorizado) - ($deuda_cuentacorrientecliente + $deuda_cuentacorrienteproveedor);
 		$array_totales = array('{periodo_actual}'=>$periodo_actual,
-							   '{estado_actual}'=>($total_facturado_int + $stock_valorizado) - ($deuda_cuentacorrientecliente + $deuda_cuentacorrienteproveedor),
+							   '{estado_actual}'=> number_format($estado_actual, 2),
 							   '{total_facturado}'=>$total_facturado,
 							   '{total_facturado_class}'=>$total_facturado_class,
 							   '{deuda_cuentacorrientecliente}'=>$deuda_cuentacorrientecliente,
