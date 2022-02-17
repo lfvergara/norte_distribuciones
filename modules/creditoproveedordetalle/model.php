eebd66aa47aa68110a4492eec130f1fe28579051
Federico Vergara <hu.ce.ro@gmail.com>
2022-02-17 18:42:30 -0300

    subiendo creditos

--- modules/creditoproveedordetalle/model.php
+++ modules/creditoproveedordetalle/model.php
@@ -1,14 +1,15 @@
 <?php
+require_once 'modules/proveedor/model.php';
 
 
 class CreditoProveedorDetalle extends StandardObject {
 	
-	function __construct() {
+	function __construct(Proveedor $proveedor=NULL) {
 		$this->creditoproveedordetalle_id = 0;
 		$this->numero = '';
 		$this->importe = 0.00;
 		$this->fecha = '';
-		$this->proveedor_id = 0;
+		$this->proveedor = $proveedor;
 	}
 }
 ?>
