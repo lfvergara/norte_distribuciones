0d10b219448cc61c2bac4f6bfd114bb0cdbf0757
Federico Vergara <hu.ce.ro@gmail.com>
2022-02-18 22:19:26 -0300

    Agregando tipo pago a cuenta corriente

--- tools/facturaPDFTool.php
+++ tools/facturaPDFTool.php
@@ -63,6 +63,7 @@ class FacturaPDF extends View {
 
         $new_array = array_chunk($egresodetalle_collection, 17);
         $contenido = '';
+        print_r($new_array);exit;
         foreach ($new_array as $egresodetalle_array) {
             $gui_facturaA = file_get_contents("static/common/plantillas_facturas/facturaA.html");
             $gui_tbl_facturaA = file_get_contents("static/common/plantillas_facturas/tbl_facturaA.html");
