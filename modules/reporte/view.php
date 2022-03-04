cff64d293cebac328391361aa1077dd3ceb6165d
Federico Vergara <hu.ce.ro@gmail.com>
2022-03-03 22:07:54 -0300

    Agregando panel rentabilidad

--- modules/reporte/view.php
+++ modules/reporte/view.php
@@ -176,7 +176,7 @@ class ReporteView extends View {
 		$gui_tbl_egreso = file_get_contents("static/modules/reporte/tbl_egreso.html");
 		$gui_tbl_egreso = $this->render_regex_dict('TBL_EGRESO', $gui_tbl_egreso, $egreso_collection);
 		$render = str_replace('{tbl_egreso}', $gui_tbl_egreso, $gui);
-		$render = $this->render($array_valores, $gui);
+		$render = $this->render($array_valores, $render);
 		$render = $this->render_breadcrumb($render);
 		$template = $this->render_template($render);
 		print $template;
