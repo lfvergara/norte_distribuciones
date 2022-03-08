47c6a642989062b67556b2b32c05cf734d6aa03e
Federico Vergara <hu.ce.ro@gmail.com>
2022-03-07 23:06:55 -0300

    Agregando libro iva compras

--- modules/reporte/controller.php
+++ modules/reporte/controller.php
@@ -2759,6 +2759,7 @@ class ReporteController {
 		$hasta = filter_input(INPUT_POST, 'hasta');
 
 		$libro_iva_compras = LibroIvaCompras::get_libro_iva_compras($desde, $hasta);
+		print_r($libro_iva_compras);exit;
 		$subtitulo = "LIBRO IVA COMPRAS: {$desde} - {$hasta}";
 		$array_encabezados = array('Fecha', 'Cla', 'Comprobante', 'Proveedor', 'CUIT', 'Neto', 'Exento', 'I.V.A.', 'IVa D/10.5', 'IVA D/27', 'Imp.Internos', 'Ret.IVA', 'Ret.IIBB', 'Per.IVA', 'Per.IIBB', 'Per.GAN', 'C.No Gravado', 'Imp TEM', 'Per IIBB', 'CF', 'Total');
 		$array_exportacion = array();
