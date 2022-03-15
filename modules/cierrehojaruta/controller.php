33073fce9e8c04273bb4eb87d1ef357de8984f73
Federico Vergara <hu.ce.ro@gmail.com>
2022-03-14 23:00:52 -0300

    Agregando interfaz para cierre de hojas de rutas

--- modules/cierrehojaruta/controller.php
+++ modules/cierrehojaruta/controller.php
@@ -30,10 +30,9 @@ class CierreHojaRutaController {
 		$this->model->get();
 
 		$select = "dchr.detallecierrehojaruta_id AS DCHRID, itp.denominacion AS TIPOPAGO, ee.denominacion AS ESTADOENTREGA, CASE WHEN eafip.egresoafip_id IS NULL THEN CONCAT((SELECT tf.nomenclatura FROM tipofactura tf WHERE e.tipofactura = tf.tipofactura_id), ' ', LPAD(e.punto_venta, 4, 0), '-', LPAD(e.numero_factura, 8, 0)) ELSE CONCAT((SELECT tf.nomenclatura FROM tipofactura tf WHERE eafip.tipofactura = tf.tipofactura_id), ' ', LPAD(eafip.punto_venta, 4, 0), '-', LPAD(eafip.numero_factura, 8, 0)) END AS FACTURA, dchr.importe AS IMPORTE";
-    	$from = "detallecierrehojaruta dchr INNER JOIN ingresotipopago ON dchr.ingresotipopago = itp.ingresotipopago_id INNER JOIN estadoentrega ee ON dchr.estadoentrega = ee.estadoentrega_id INNER JOIN egreso e ON dchr.egreso_id = e.egreso_id LEFT JOIN egrosafip eafip ON e.egreso_id = eafip.egreso_id";
+    	$from = "detallecierrehojaruta dchr INNER JOIN ingresotipopago itp ON dchr.ingresotipopago = itp.ingresotipopago_id INNER JOIN estadoentrega ee ON dchr.estadoentrega = ee.estadoentrega_id INNER JOIN egreso e ON dchr.egreso_id = e.egreso_id LEFT JOIN egresoafip eafip ON e.egreso_id = eafip.egreso_id";
     	$where = "dchr.cierrehojaruta_id = {$cierrehojaruta_id}";
     	$detallecierrehojaruta_collection = CollectorCondition()->get('DetalleCierreHojaRuta', $where, 4, $from, $select);
-    	print_r($detallecierrehojaruta_collection);exit;
     	$this->view->consultar($detallecierrehojaruta_collection, $this->model);
 	}	
 }
