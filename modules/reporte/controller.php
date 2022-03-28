f1e8971e9d24c46d55a2ad5154a90f46c144ad22
Federico Vergara <hu.ce.ro@gmail.com>
2022-03-28 18:54:43 -0300

    Habilitando opción de IIBB en clientes

--- modules/reporte/controller.php
+++ modules/reporte/controller.php
@@ -2729,10 +2729,10 @@ class ReporteController {
 		fclose($fp);
 		header('Content-Description: File Transfer');
 		header('Content-Disposition: attachment; filename='.$archivo;
+		/*
 		header('Expires: 0');
 		header('Cache-Control: must-revalidate');
 		header('Pragma: public');
-		/*
 		header('Content-Length: ' . filesize($archivo));
 		header("Content-Type: text/plain");
 		readfile($archivo);
