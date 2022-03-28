f10b8b90e61700561f3754ac10ac5c972778abbf
Federico Vergara <hu.ce.ro@gmail.com>
2022-03-28 19:30:52 -0300

    Descarga de txt SAP

--- modules/reporte/controller.php
+++ modules/reporte/controller.php
@@ -2730,14 +2730,13 @@ class ReporteController {
 		fclose($fp);
 		header('Content-Description: File Transfer');
 		header('Content-Disposition: attachment; filename='.$archivo);
-		/*
 		header('Expires: 0');
 		header('Cache-Control: must-revalidate');
 		header('Pragma: public');
-		header('Content-Length: ' . filesize($archivo));
+		header('Content-Length: ' . filesize($directorio . $archivo));
 		header("Content-Type: text/plain");
 		readfile($archivo);
-		*/
+		
 		exit;
 	}
 
